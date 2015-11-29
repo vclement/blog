9e45ff3651a0157d64773e9e233cf273f82586ed
Vince <vincet@debian>
2015-11-29 15:04:22 +0100

    Ajout du formulaire

--- src/ArticleBundle/Controller/ArticleController.php
+++ src/ArticleBundle/Controller/ArticleController.php
@@ -6,6 +6,7 @@
 
 namespace ArticleBundle\Controller;
 
+use ArticleBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,114 +21,143 @@ class ArticleController extends Controller
 	//mais c'est aussi la page qui permet de voir tout les articles.
 	
     public function indexAction($page){
-    	//Maintenant on va commencer la page d'accueil du blog.
-    	
-    	//Si la page est inférieur à 1, il y a une erreur
-    	if($page < 1){
-    		// On déclenche l'exeption et on ouvre une page personnalisé d'erreur 404
-    		throw new NotFoundHttpException('Page "'.$page.'" innexistante.');
-    	}
+    	//Maintenant on va commencer la page d'accueil du blog
     	
     	// Puis on récupèrera tout les articles de blog qui sont déjà crée
     	
-    	// Notre liste d'annonce en dur
-		$listArticles = array(
-		  array(
-			'title'   => 'Recherche développpeur Symfony2',
-			'id'      => 1,
-			'author'  => 'Alexandre',
-			'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-			'date'    => new \Datetime()),
-		  array(
-			'title'   => 'Mission de webmaster',
-			'id'      => 2,
-			'author'  => 'Hugo',
-			'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-			'date'    => new \Datetime()),
-		  array(
-			'title'   => 'Offre de stage webdesigner',
-			'id'      => 3,
-			'author'  => 'Mathieu',
-			'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-			'date'    => new \Datetime())
-		);
+    	// Pour récupérer la liste de toutes les annonces : on utilise findAll()
+   		$listArticles = $this->getDoctrine()
+      ->getManager()
+      ->getRepository('ArticleBundle:Article')
+      ->findAll()
+    ;
     	
     	// Mais pour le moment on va simplement utiliser le template
-    	return $this->render('ArticleBundle:ArticleBlog:index.html.twig', array('listArticles' => $listArticles
-    	));
+    	return $this->render('ArticleBundle:ArticleBlog:index.html.twig', array('listArticles' => $listArticles));
+    	
     }
 	
 		
 	// La route fait appel à ArticleBundle:ArticleBlog:view,
 	// on doit donc définir la méthode viewAction.
 	// Cette méthode nous permet de voir un article en particulier.
-	public function voirAction($id){
-		// On récupère chaque article grâce à son id.
-		
-		return $this->render('ArticleBundle:ArticleBlog:index.html.twig', array(
-  'listArticles' => array()
-));
+	public function voirAction($id)
+	{
+		// On récupère le repository
+		$repository = $this->getDoctrine()
+		  ->getManager()
+		  ->getRepository('ArticleBundle:Article')
+		;
+
+		// On récupère l'entité correspondante à l'id $id
+		$article = $repository->find($id);
+
+		// $advert est donc une instance de ArticleBundle\Entity\Article
+		// ou null si l'id $id  n'existe pas, d'où ce if :
+		if (null === $article) {
+		  throw new NotFoundHttpException("L'article d'id ".$id." n'existe pas.");
+		}
+
+		// Le render ne change pas, on passait avant un tableau, maintenant un objet
+		return $this->render('ArticleBundle:ArticleBlog:voir.html.twig', array(
+		  'article' => $article));
+	
 	}
 
 
 		// La méthode addAction, va permettre l'ajout d'un article sur notre site de blog.		
-	public function ajoutAction(Request $request){
+	public function ajoutAction(Request $request)
+	{
+		// Création de l'entité
+		$article = new Article();
+		$article->setDate(new \Datetime());
+		$article->setTitle('Mettre le titre ici');
+		$article->setAuthor('Qui êtes-vous ?');
+		$article->setContent("Mettez ici la description de l'article.");
 		
-		// C'est ici que l'on gère l'ajout du formulaire.
-		$session = $request->getSession();
+		// On peut ne pas définir ni la date ni la publication,
+		// car ces attributs sont définis automatiquement dans le constructeur
+
+		// On crée le formbuilder grace au service form factory
+		$formBuilder = $this->get('form.factory')->createBuilder('form', $article);
 		
-		//On vérifie que l'utilisateur a bien utilisé la méthode POST
-    	if('POST' == $request->getMethod()){
-			//Ici on a l'interface de création de l'article puis après l'envoie, on affiche l'article qui vient d'être crée.
-			$request->getSession()->getFlashBag()->add('notice', 'Article créé.');
+		//On ajoute les champs de l'entité que l'on veut à notre formulaire
+		$formBuilder
+			->add('title',       'text')
+			->add('content',     'textarea')
+			->add('author',      'text')
+			->add('published',   'checkbox')
+			->add('save',        'submit')
+		;
+		//A partir du formbuilder on génère le formulaire
+		$form = $formBuilder->getForm();
+		
+		$form->HandleRequest($request);
+		
+		if($form->isValid()) {
+			//On l'enregistre en base de donnée
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($article);
+			$em->flush();
 			
-			//Puis on va redirige vers l'article.
-			return $this->redirect($this->generateUrl('blog_voir', array('id' => 5)));		
+			$request->getSession()->getFlashBag()->add('notice', 'Article enregistré.');
+			
+			return $this->redirect($this->generateUrl('article_voir', array('id' => $article->getId())));
 		}
-		
-		// En cas d'absence de méthode POST on redirige vers le formulaire.
-		return $this->render('ArticleBundle:ArticleBlog:ajout.html.twig');
-	}
-	
-	
-	// La fonction suivante va modifier un article qui a déjà été crée.
-	public function modifieAction(){
-		// D'abord on récupère l'id de l'article à modifier.
-		
-		// Puis une fois modifier, on envoie les modifications.
-		if('POST' == $request->getMethod()){
-			$request->getSession()->getFlashBag()->add('notice', 'Article modifié.');
-			return $this->redirect($this->generateUrl('blog_voir', array('id' => 5)));
-		}
-		
-		return $this->render('ArticleBundle:ArticleBlog:modifie.html.twig');
+
+		// Reste de la méthode qu'on avait déjà écrit
+		//if ($request->isMethod('POST')) {
+		//  $request->getSession()->getFlashBag()->ajout('article', 'Article bien enregistré.');
+		// return $this->redirect($this->generateUrl('article_voir', array('id' => $article->getId())));}
+
+		return $this->render('ArticleBundle:ArticleBlog:ajout.html.twig', array('form' => $form->createView(), ));
 	}
     
     
     // Cette fonction permet de supprimer un article qui a déjà été créée
-    public function supprimeAction(){
+    public function supprimeAction(Request $request){
     	// Tout d'abord, il faudra récupérer l'id de l'article.
+    	$article = new Article();
     	
+    	$article->getId();
     	// Ensuite, on gère la suppression de la base de donnée.
     	
     	return $this->render('ArticleBundle:ArticleBlog:supprime.html.twig');    	
     }
     
-    public function menuAction($limit)
+    public function menuAction($limit = 3)
 	{
-	// On fixe en dur une liste ici, bien entendu par la suite
-	// on la récupérera depuis la BDD !
-	 $listArticles = array(
-      array('id' => 2, 'title' => 'Ouverture du blog'),
-      array('id' => 5, 'title' => 'Mon premier article'),
-      array('id' => 9, 'title' => 'Ce qui va suive dans ce blog')
-    );
+		$listArticles = $this->getDoctrine()
+		  ->getManager()
+		  ->getRepository('ArticleBundle:Article')
+		  ->findBy(
+		    array(),                 // Pas de critère
+		    array('date' => 'desc'), // On trie par date décroissante
+		    $limit,                  // On sélectionne $limit annonces
+		    0                        // À partir du premier
+		);
 
-	return $this->render('ArticleBundle:ArticleBlog:menu.html.twig', array(
-	  // Tout l'intérêt est ici : le contrôleur passe les variables nécessaires au template !
-	  'listArticles' => $listArticles
+		return $this->render('ArticleBundle:ArticleBlog:menu.html.twig', array(
+		  'listArticles' => $listArticles
+		));
+	}
+	
+	public function modifieAction($id, Request $request)
+	{
+	// Voilà la fonction qui permet de modifier un article
+	$em = $this->getDoctrine()->getManager();
+
+	$article = $em->getRepository('ArticleBundle:Article')->find($id);
+	
+	$formBuilder = $this->get('form.factory')->createBuilder('form', $article);
+	$form = $formBuilder->getForm();
+		
+	$form->HandleRequest($request);
+
+	return $this->render('ArticleBundle:ArticleBlog:modifie.html.twig', array(
+	  'article' => $article
 	));
 	}
-   
+
 }
 
